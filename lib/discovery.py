8f43ffd340774c144870641c481c0cc4de7c03f5
dala318 <daniel@vikstrom.name>
2022-05-24 19:35:00 +0200

    First commit

---
+++ lib/discovery.py
@@ -0,0 +1,15 @@
+include socket
+
+class Discover:
+    def __init__(self):
+        print('Init')
+        pass
+
+    def run():
+        print('Running')
+        pass
+
+
+if __name__ == __main__:
+    discover = Discover()
+    discover.run()
