dd643fd46383ed6346b2a4bafe7a3bc9b2618aae
Daniel Vikström <daniel@vikstrom.name>
2022-05-26 18:37:28 +0000

    Clean up discovery and H.A. style

--- lib/device.py
+++ lib/device.py
@@ -1,7 +1,4 @@
 import socket
-import asyncio
-import time
-from sys import argv
 
 PH803W_DEFAULT_TCP_PORT = 12416
 PH803W_PING_INTERVAL = 4000
@@ -13,36 +10,42 @@ class Device(object):
     def __init__(self, host):
         self.result = {}
         self.host = host
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._loop = True
 
     async def run_async(self):
         self.run()
 
-    def run(self):
-        self.loop = True
-        self.socket.connect((self.host, PH803W_DEFAULT_TCP_PORT))
+    def run(self, once: bool = False) -> bool:
+        self._loop = True
+        self._socket.connect((self.host, PH803W_DEFAULT_TCP_PORT))
 
-        data = bytes.fromhex('0000000303000006')
-        self.socket.sendall(data)
-        response = self.socket.recv(1024)
+        data = bytes.fromhex("0000000303000006")
+        self._socket.sendall(data)
+        response = self._socket.recv(1024)
         passcode_lenth = response[9]
         passcode_raw = response[10 : 10 + passcode_lenth]
         passcode = passcode_raw.decode("utf-8")
-        print(passcode)
+        # print(passcode)
 
-        data = bytes.fromhex('000000030f00000800') + passcode_lenth.to_bytes(1, 'little') + passcode_raw
-        self.socket.sendall(data)
-        response = self.socket.recv(1024)
+        data = (
+            bytes.fromhex("000000030f00000800")
+            + passcode_lenth.to_bytes(1, "little")
+            + passcode_raw
+        )
+        self._socket.sendall(data)
+        response = self._socket.recv(1024)
         if response[8] != 0:
-            print('Error connecting')
+            #     print("Error connecting")
+            return False
 
         # Connection established, from now on some cyclig bahavior
-        data = bytes.fromhex('000000030400009002')
-        self.socket.sendall(data)
+        data = bytes.fromhex("000000030400009002")
+        self._socket.sendall(data)
         empty_counter = 0
-        data = bytes.fromhex('0000000303000015')
-        while self.loop and empty_counter < 10:
-            response = self.socket.recv(1024)
+        data = bytes.fromhex("0000000303000015")
+        while self._loop and empty_counter < 10:
+            response = self._socket.recv(1024)
             if len(response) == 0:
                 empty_counter += 1
                 continue
@@ -51,44 +54,58 @@ class Device(object):
             if len(response) == 18:
                 flag1 = response[8]
                 if flag1 & 0b0000_0100:
-                    print('In water')
+                    print("In water")
                 flag2 = response[9]
                 if flag2 & 0b0000_0010:
-                    print('ORP on')
+                    print("ORP on")
                 if flag2 & 0b0000_0001:
-                    print('PH on')
-                #state_raw = response[8 : 9]
-                ph_raw = response[10 : 12]
-                ph = int.from_bytes(ph_raw, 'big') * 0.01
-                redox_raw = response[12 : 14]
-                redox = int.from_bytes(redox_raw, 'big') - 2000
-                unknown1_raw = response[14 : 16]
-                unknown1 = int.from_bytes(unknown1_raw, 'big')
-                unknown2_raw = response[15 : 18]
-                unknown2 = int.from_bytes(unknown2_raw, 'big')
-                print('pH: %s, Redox: %s, U1: %s, U2: %s' % (ph, redox, unknown1, unknown2), flush=True)
-
-
-
-            self.socket.sendall(data)
-            response = self.socket.recv(1024)
-
+                    print("PH on")
+                # state_raw = response[8 : 9]
+                ph_raw = response[10:12]
+                ph = int.from_bytes(ph_raw, "big") * 0.01
+                redox_raw = response[12:14]
+                redox = int.from_bytes(redox_raw, "big") - 2000
+                unknown1_raw = response[14:16]
+                unknown1 = int.from_bytes(unknown1_raw, "big")
+                unknown2_raw = response[15:18]
+                unknown2 = int.from_bytes(unknown2_raw, "big")
+                print(
+                    "pH: %s, Redox: %s, U1: %s, U2: %s"
+                    % (ph, redox, unknown1, unknown2),
+                    flush=True,
+                )
+            self._socket.sendall(data)
+            response = self._socket.recv(1024)
+            if once:
+                break
 
     def _handle_response(self, data):
         if data[0] != 0 and data[1] != 0 and data[2] != 0 and data[2] != 3:
-            print('Ignore data package because invalid prefix: %s' % data[0:3])
-            self.result['status'] = ['Error', 'Ignore data package because invalid prefix']
+            # print("Ignore data package because invalid prefix: %s" % data[0:3])
+            self.result["status"] = [
+                "Error",
+                "Ignore data package because invalid prefix",
+            ]
             return
         data_length = data[4]
         if len(data) != data_length + 5:
             if len(data) > data_length:
-                additional_data = data[data_length: len(data)]
-                data = data[0 : data_length]
-                print('Split into two data packages because additional data detected. First %s - Second %s}' % (data.toString('hex'), additional_data.toString('hex')))
+                additional_data = data[data_length : len(data)]
+                data = data[0:data_length]
+                # print(
+                #     "Split into two data packages because additional data detected. First %s - Second %s}"
+                #     % (data.toString("hex"), additional_data.toString("hex"))
+                # )
                 self._handle_response(additional_data)
             else:
-                print('Ignore data package because invalid length(%s): %s' % (data_length, data))
-                self.result['status'] = ['Error', 'Ignore data package because invalid length']
+                # print(
+                #     "Ignore data package because invalid length(%s): %s"
+                #     % (data_length, data)
+                # )
+                self.result["status"] = [
+                    "Error",
+                    "Ignore data package because invalid length",
+                ]
                 return
 
         message_type = data[7]
@@ -103,20 +120,32 @@ class Device(object):
         elif message_type == 0x94:
             self._handle_data_extended_response(data)
         else:
-            print('Ignore data package because invalid message type %s: %s' % (message_type, data))
-            self.result['status'] = ['Ignore', 'Ignore data package because invalid length', message_type, data]
+            # print(
+            #     "Ignore data package because invalid message type %s: %s"
+            #     % (message_type, data)
+            # )
+            self.result["status"] = [
+                "Ignore",
+                "Ignore data package because invalid length",
+                message_type,
+                data,
+            ]
 
     def _handle_passcode_response(self, data):
         pass
+
     def _handle_login_response(self, data):
         pass
+
     def _handle_ping_pong_response(self):
         pass
+
     def _handle_data_extended_response(self, data):
         pass
 
     def _send_ping(self):
         pass
+
     #     if (this.pingWaitTimeout) {
     #         clearTimeout(this.pingWaitTimeout);
     #         this.pingWaitTimeout = null;
@@ -133,10 +162,10 @@ class Device(object):
     # }    def _handle_data_response(self, data):
 
     def abort(self):
-        self.loop = False
+        self._loop = False
 
     def close(self):
-        self.socket.close()
+        self._socket.close()
 
     def get_result(self):
         return str(self.result)
@@ -146,4 +175,4 @@ class Device(object):
         return self
 
     def __exit__(self, type, value, traceback):
-        self.socket.close()
+        self._socket.close()
--- lib/discovery.py
+++ lib/discovery.py
@@ -4,77 +4,57 @@ import asyncio
 PH803W_UDP_PORT = 12414
 
 
+class DiscoveryError(ConnectionError):
+    pass
+
+
 class Discovery(object):
     def __init__(self):
-        self.result = {}
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.device = None
+        self._socket = socket.socket(
+            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+        )
         # Enable broadcasting mode
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         # Set a timeout so the socket does not block
         # indefinitely when trying to receive data.
-        self.socket.settimeout(1)
+        self._socket.settimeout(1)
 
     async def run_async(self):
         self.run()
 
     def run(self):
-        data = bytes.fromhex('0000000303000003')
-        self.socket.sendto(data, ('<broadcast>', PH803W_UDP_PORT))
-        print("Sent request message!")
-        
-        data, remote = self.socket.recvfrom(1024)
+        # Send dicovery request broadcast
+        data = bytes.fromhex("0000000303000003")
+        self._socket.sendto(data, ("<broadcast>", PH803W_UDP_PORT))
+        # print("Sent request message!")
 
+        # Receive device reaponse to discovery
+        data, remote = self._socket.recvfrom(1024)
         if data[0] != 0 and data[1] != 0 and data[2] != 0 and data[2] != 3:
-            print('Ignore data package because invalid prefix: %s' % data[0:3])
-            self.result['status'] = ['Error', 'Ignore data package because invalid prefix']
-            return
+            # print('Ignore data package because invalid prefix: %s' % data[0:3])
+            raise DiscoveryError("Ignore data package because invalid prefix")
         data_length = data[4]
         if len(data) != data_length + 5:
-            print('Ignore data package because invalid length(%s): %s' % (data_length, data))
-            self.result['status'] = ['Error', 'Ignore data package because invalid length']
-            return
+            # print('Ignore data package because invalid length(%s): %s' % (data_length, data))
+            raise DiscoveryError("Ignore data package because invalid length")
         if data[7] == 3:
-            self.result['status'] = ['Unknown']
-            return
+            raise DiscoveryError("Unknown response message type")
         if data[7] != 4:
-            print('Ignore data package because invalid message type ${data[7]}')
-            self.result['status'] = ['Error', 'Ignore data package because invalid message type']
-            return
+            # print("Ignore data package because invalid message type ${data[7]}")
+            raise DiscoveryError("Ignore data package because invalid message type")
 
-        print('Parsing discovered device: %s: %s - %s' % (remote[0], remote[1], data[7:]))
-        self.result['status'] = ['Error']  # Temporary until all pass
-        self.result['result'] = {'ip' : remote[0]}
-
-        id1_length = data[9]
-        id1_raw = data[10 : 10 + id1_length]
-        self.result['result']['id1'] = id1_raw.decode("utf-8")
-
-        id2_length = data[9 + id1_length + 12]
-        id2_raw = data[9 + id1_length + 13 : 9 + id1_length + 13 + id2_length]
-        self.result['result']['id2'] = id2_raw.decode("utf-8")
-
-        idx = 9 + id1_length + 13 + id2_length + 8
-        idx_start = idx
-        while data[idx] != 0:
-            idx += 1
-        api_server_raw = data[idx_start: idx]
-        self.result['result']['api_server'] = api_server_raw.decode("utf-8")
-
-        idx += 1
-
-        idx_start = idx
-        while data[idx] != 0:
-            idx += 1
-        version_raw = data[idx_start: idx]
-        self.result['result']['version_server'] = version_raw.decode("utf-8")
-
-        self.result['status'] = ['Success']
+        # Parsing result of correct type
+        # print(
+        #     "Parsing discovered device: %s: %s - %s" % (remote[0], remote[1], data[7:])
+        # )
+        self.device = Device(remote[0], data)
 
     def close(self):
-        self.socket.close()
+        self._socket.close()
 
     def get_result(self):
-        return self.result
+        return self.device
 
     def __enter__(self):
         self.run()
@@ -82,3 +62,30 @@ class Discovery(object):
 
     def __exit__(self, type, value, traceback):
         self.close()
+
+
+class Device:
+    def __init__(self, ip: str, data):
+        self.ip = ip
+
+        id1_length = data[9]
+        id1_raw = data[10 : 10 + id1_length]
+        self.id1 = id1_raw.decode("utf-8")
+
+        id2_length = data[9 + id1_length + 12]
+        id2_raw = data[9 + id1_length + 13 : 9 + id1_length + 13 + id2_length]
+        self.id2 = id2_raw.decode("utf-8")
+
+        idx = 9 + id1_length + 13 + id2_length + 8
+        idx_start = idx
+        while data[idx] != 0:
+            idx += 1
+        api_server_raw = data[idx_start:idx]
+        self.api_server = api_server_raw.decode("utf-8")
+
+        idx += 1
+        idx_start = idx
+        while data[idx] != 0:
+            idx += 1
+        version_raw = data[idx_start:idx]
+        self.version_server = version_raw.decode("utf-8")
